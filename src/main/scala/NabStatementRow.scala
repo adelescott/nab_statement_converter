09a53483dc55ef5380a2d9ce4777436cdcec121f
Adele Scott <adele.frances.scott@gmail.com>
2023-08-09 20:57:05 +1000

    Switched to using a HeaderDecoder now that NAB csvs have a proper header

--- src/main/scala/NabStatementRow.scala
+++ src/main/scala/NabStatementRow.scala
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import kantan.csv.{RowDecoder, RowEncoder}
+import kantan.csv.{HeaderDecoder, RowEncoder}
 
 /** Class to represent a row of an Nab statement csv.
   */
@@ -13,40 +13,18 @@ case class NabStatementRow(
 
 object NabStatementRow {
 
-  // Unfortunately the header row is incorrect in the NAB files, othewise this would be a
-  // better implementation.
-  //
-  // implicit val nabStatementRowDecoder: HeaderDecoder[NabStatementRow] =
-  //   HeaderDecoder.decoder(
-  //     "Date",
-  //     "Amount",
-  //     "Transaction Details"
-  //   )((date: String, amount: BigDecimal, description: String) =>
-  //     NabStatementRow(
-  //       stringToDate(date),
-  //       amount,
-  //       description
-  //     )
-  //   )
-
-  implicit val nabStatementRowDecoder: RowDecoder[NabStatementRow] =
-    RowDecoder.ordered {
-      (
-          date: String,
-          amount: BigDecimal,
-          _: String, /* Account number */
-          _: String, /* Transaction Type */
-          description: String, /* Transaction Details */
-          _: String, /* Balance */
-          _: String, /* Category */
-          _: String /* Merchant Name */
-      ) =>
-        NabStatementRow(
-          stringToDate(date),
-          amount,
-          description
-        )
-    }
+  implicit val nabStatementRowDecoder: HeaderDecoder[NabStatementRow] =
+    HeaderDecoder.decoder(
+      "Date",
+      "Amount",
+      "Transaction Details"
+    )((date: String, amount: BigDecimal, description: String) =>
+      NabStatementRow(
+        stringToDate(date),
+        amount,
+        description
+      )
+    )
 
   implicit val nabStatementRowEncoder: RowEncoder[NabStatementRow] =
     RowEncoder.ordered { nabStatementRow: NabStatementRow =>
